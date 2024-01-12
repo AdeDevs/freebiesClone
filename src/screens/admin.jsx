0d0445cebcda46668a503aa26a21d98865cc85df
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-01-12 14:44:14 +0100

    updt

--- src/screens/admin.jsx
+++ src/screens/admin.jsx
@@ -1,4 +1,5 @@
 import "../styles/admin.css"
+import Articles from "./articles";
 
 function AdminPage() {
   return (
@@ -9,14 +10,19 @@ function AdminPage() {
           name="article"
           placeholder="Type your article in here"
         ></textarea>
-        <label for="categories">Select Category: </label>
+        <div className="categories">
+        <p >Select Category: </p>
         <select name="categories">
           <option value="update">Update</option>
           <option value="crypto">Crypto</option>
           <option value="airtime">Airtime</option>
         </select>
+        </div>
         <button>Post Article</button>
       </form>
+      <div className="boxes">
+        {/* <AdminArticles /> */}
+      </div>
     </div>
   );
 }
