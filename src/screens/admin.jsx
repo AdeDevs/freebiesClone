204f52146b48defbd63901879f7bdabfa61bb807
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-01-14 00:14:36 +0100

    clee

--- src/screens/admin.jsx
+++ src/screens/admin.jsx
@@ -3,26 +3,8 @@ import Articles from "./articles";
 
 function AdminPage() {
   return (
-    <div className="add-article">
-      <form>
-        <input type="text" placeholder="Type in your title" />
-        <textarea
-          name="article"
-          placeholder="Type your article in here"
-        ></textarea>
-        <div className="categories">
-        <p >Select Category: </p>
-        <select name="categories">
-          <option value="update">Update</option>
-          <option value="crypto">Crypto</option>
-          <option value="airtime">Airtime</option>
-        </select>
-        </div>
-        <button>Post Article</button>
-      </form>
-      <div className="boxes">
-        {/* <AdminArticles /> */}
-      </div>
+    <div className="admin-page">
+      
     </div>
   );
 }
