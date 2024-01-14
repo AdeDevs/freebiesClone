b6e35f644e18b8fc34997f78ccf4dc98d57e2962
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-01-14 12:28:23 +0100

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,17 @@ import Airtime from "./screens/Airtime"
 import ErrorBoundary from "./screens/ErrorBoundary"
 import Recent from "./screens/recent";
 import FullArticle from "./screens/articlepage";
+import AdminPage from "./admin/admin";
+import AdminLogin from "./admin/adminlogin";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      {/* <NavBar /> */}
       <div className="main">
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<AdminLogin />}></Route>
+        <Route path="/admin" element={<AdminPage />}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/update" element={<Update/>}></Route>
         <Route path="/crypto" element={<Crypto/>}></Route>
--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -1,10 +1,10 @@
-import "../styles/admin.css"
+import "./admin.css"
 import Articles from "../screens/articles";
 
 function AdminPage() {
   return (
     <div className="admin-page">
-      
+      <h1>Hello</h1>
     </div>
   );
 }
